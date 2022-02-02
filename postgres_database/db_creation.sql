fcc4181f2ba2c70672f57716d3d5a56d1bf677ef
tobiedesgreniers <tobiedesgreniers300@gmail.com>
2022-02-02 13:57:27 -0500

    ERD + initial SQL scripts

---
+++ postgres_database/db_creation.sql
@@ -0,0 +1,5 @@
+-- To run this file on postgreSQL server, use the "\i" command with the path to this file as argument using double backslashes
+-- examples:
+-- \i 'C:\\projects\\training\\postgres_database\\db_creation.sql
+
+DROP DATABASE IF EXISTS stocks;
